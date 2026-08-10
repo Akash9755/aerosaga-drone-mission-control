2ae5d741b88940c4f4291d1a81c424ba1eba7fd7
Djoshi30 <dhjoshi03@gmail.com>
2026-08-10 23:42:50 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/aerosaga/AerosagaApplication.java
@@ -0,0 +1,13 @@
+package com.aerosaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerosagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerosagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aerosaga/AerosagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aerosaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerosagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
