8906068d31a8b81f211cbcf0485581978c7eb6c8
Djoshi30 <dhjoshi03@gmail.com>
2026-08-15 22:28:09 +0530

    feat: add takeoff activity

---
+++ src/main/java/com/aerosaga/activity/DroneActivityImpl.java
@@ -0,0 +1,9 @@
+package com.aerosaga.activity;
+
+public class DroneActivityImpl implements DroneActivity {
+
+    @Override
+    public void takeoff() {
+        System.out.println("Drone takeoff activity started");
+    }
+}
