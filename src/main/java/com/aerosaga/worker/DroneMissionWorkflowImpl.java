f154ca561c8915d2e740bf5650b62259a8a1d236
Djoshi30 <dhjoshi03@gmail.com>
2026-08-15 22:32:28 +0530

    feat: integrate takeoff workflow

--- src/main/java/com/aerosaga/worker/DroneMissionWorkflowImpl.java
+++ src/main/java/com/aerosaga/worker/DroneMissionWorkflowImpl.java
@@ -1,11 +1,24 @@
 package com.aerosaga.worker;
 
+import com.aerosaga.activity.DroneActivity;
 import com.aerosaga.workflow.DroneMissionWorkflow;
+import io.temporal.activity.ActivityOptions;
+import io.temporal.workflow.Workflow;
+
+import java.time.Duration;
 
 public class DroneMissionWorkflowImpl implements DroneMissionWorkflow {
 
+    private final DroneActivity droneActivity =
+            Workflow.newActivityStub(
+                    DroneActivity.class,
+                    ActivityOptions.newBuilder()
+                            .setStartToCloseTimeout(Duration.ofMinutes(1))
+                            .build()
+            );
+
     @Override
     public void executeMission() {
-        // Mission steps will be added in later commits.
+        droneActivity.takeoff();
     }
 }
