c407ad3fd9013123a4234803f1db3277fcc45216
Djoshi30 <dhjoshi03@gmail.com>
2026-08-18 22:59:37 +0530

    feat: add saga compensation

--- src/main/java/com/aerosaga/worker/DroneMissionWorkflowImpl.java
+++ src/main/java/com/aerosaga/worker/DroneMissionWorkflowImpl.java
@@ -1,5 +1,6 @@
 package com.aerosaga.worker;
 
+import com.aerosaga.activity.DeliveryActivity;
 import com.aerosaga.activity.DroneActivity;
 import com.aerosaga.workflow.DroneMissionWorkflow;
 import io.temporal.activity.ActivityOptions;
@@ -23,10 +24,31 @@ public class DroneMissionWorkflowImpl implements DroneMissionWorkflow {
                             .build()
             );
 
+    private final DeliveryActivity deliveryActivity =
+            Workflow.newActivityStub(
+                    DeliveryActivity.class,
+                    ActivityOptions.newBuilder()
+                            .setStartToCloseTimeout(Duration.ofMinutes(1))
+                            .setRetryOptions(
+                                    RetryOptions.newBuilder()
+                                            .setMaximumAttempts(3)
+                                            .build()
+                            )
+                            .build()
+            );
+
     @Override
     public void executeMission() {
         droneActivity.takeoff();
         droneActivity.navigateToPickup();
+
+        try {
+            deliveryActivity.dropPackage();
+        } catch (Exception e) {
+            droneActivity.returnToBase();
+            throw e;
+        }
+
         droneActivity.returnToBase();
     }
 }
