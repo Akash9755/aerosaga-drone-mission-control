9f19ebd8c03534b4caac42433dd34f7080dfb64a
Djoshi30 <dhjoshi03@gmail.com>
2026-08-20 22:20:42 +0530

    feat: add mission control service

---
+++ src/main/java/com/aerosaga/service/MissionControlService.java
@@ -0,0 +1,45 @@
+package com.aerosaga.service;
+
+import com.aerosaga.workflow.DroneMissionWorkflow;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.serviceclient.WorkflowServiceStubsOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MissionControlService {
+
+    private static final String TASK_QUEUE = "DRONE_MISSION_TASK_QUEUE";
+    private static final String WORKFLOW_ID = "drone-mission-001";
+
+    private final DroneMissionWorkflow workflow;
+
+    public MissionControlService() {
+        WorkflowServiceStubs service =
+                WorkflowServiceStubs.newServiceStubs(
+                        WorkflowServiceStubsOptions.newBuilder()
+                                .setTarget("localhost:7233")
+                                .build()
+                );
+
+        WorkflowClient client = WorkflowClient.newInstance(service);
+
+        this.workflow =
+                client.newWorkflowStub(
+                        DroneMissionWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setTaskQueue(TASK_QUEUE)
+                                .setWorkflowId(WORKFLOW_ID)
+                                .build()
+                );
+    }
+
+    public void abortMission() {
+        workflow.abortMission();
+    }
+
+    public void returnHome() {
+        workflow.returnHome();
+    }
+}
