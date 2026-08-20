ef31ba720bea077eacac38f7393fb3a712872221
Djoshi30 <dhjoshi03@gmail.com>
2026-08-20 22:23:20 +0530

    feat: add mission control REST API

---
+++ src/main/java/com/aerosaga/controller/MissionController.java
@@ -0,0 +1,30 @@
+package com.aerosaga.controller;
+
+import com.aerosaga.service.MissionControlService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/missions")
+public class MissionController {
+
+    private final MissionControlService missionControlService;
+
+    public MissionController(MissionControlService missionControlService) {
+        this.missionControlService = missionControlService;
+    }
+
+    @PostMapping("/abort")
+    public ResponseEntity<String> abortMission() {
+        missionControlService.abortMission();
+        return ResponseEntity.ok("Mission abort requested");
+    }
+
+    @PostMapping("/return-home")
+    public ResponseEntity<String> returnHome() {
+        missionControlService.returnHome();
+        return ResponseEntity.ok("Return home requested");
+    }
+}
